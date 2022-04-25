71c54605c764bc13c6878b7f2038f411bef102e3
seosh5704 <seosh5704@gmail.com>
2022-04-25 20:52:35 +0900

    Initial Commit

---
+++ FindCoronaCenter/FindCoronaCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterApp.swift
+//  FindCoronaCenter
+//
+//  Created by seosh on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
